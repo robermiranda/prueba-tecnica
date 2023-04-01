4592b0a910ed791f457c76b56ef8a7662acd1cfb
Rober Miranda <rob.mirandam@gmail.com>
2023-04-01 01:46:03 -0600

    using axios create to get request

--- src/util/net.ts
+++ src/util/net.ts
@@ -1,16 +1,20 @@
 import axios from 'axios';
 import { bankT } from './bankTypes';
 
-const URL: string = 'https://dev.obtenmas.com/catom/api/challenge/banks';
+//const BASE_URL: string = 'https://dev.obtenmas.com/catom/api/challenge/banks';
+const BASE_URL: string = 'https://dev.obtenmas.com';
+
+const http = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+    }
+});
 
 export default async function fetchBankList (): Promise<bankT[]> {
     try {
-        const response = await axios.get(URL, {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-            }
-        });
+        const response = await http.get('/catom/api/challenge/banks');
         return response.data;
     }
     catch (error) {
