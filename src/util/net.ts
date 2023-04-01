dcc932d2a02141e820b074d91b7d86be325754a0
Rober Miranda <rob.mirandam@gmail.com>
2023-04-01 01:22:33 -0600

    Add headers to axios get request

--- src/util/net.ts
+++ src/util/net.ts
@@ -5,7 +5,12 @@ const URL: string = 'https://dev.obtenmas.com/catom/api/challenge/banks';
 
 export default async function fetchBankList (): Promise<bankT[]> {
     try {
-        const response = await axios.get(URL);
+        const response = await axios.get(URL, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            }
+        });
         return response.data;
     }
     catch (error) {
