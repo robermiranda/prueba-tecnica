42337fde2a1e98832fac997a45813757c05c0b8d
Rober Miranda <rob.mirandam@gmail.com>
2023-04-02 21:53:28 -0600

    Add OnePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from 'react';
 import {RecoilRoot} from 'recoil';
-import BankList from './components/BankList';
+//import BankList from './components/BankList';
 import { Box } from '@mui/material';
 import BankCard from './components/BankCard';
 import Home from './components/Home';
 import Page404 from './components/Page404';
+import OnePage from './components/OnePage';
 import {
     BrowserRouter as Router,
     Route,
@@ -19,7 +20,7 @@ function App() {
             <Suspense fallback={<p>Loading Data . . . . .</p>}>
                 <Routes>
                     <Route path='/' element={ <Home/> }/>
-                    <Route path='/bankList' element={ <BankList/> }/>
+                    <Route path='/bankList' element={ <OnePage/> }/>
                     <Route path='/bank/:id' element={ <BankCard/> }/>
                     <Route path='*' element={ <Page404/> }/>
                 </Routes>
