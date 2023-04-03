f2065934983871076ea86f0c97f68038c91e98c8
Rober Miranda <rob.mirandam@gmail.com>
2023-04-02 23:49:52 -0600

    Modifying margins

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material';
 import OnePage from './components/OnePage';
 
 export default function App() {
-    return <Box mx={2} mt={3}>
+    return <Box mx={{md: 2}} mt={{md:3}}>
         <RecoilRoot>
             <Suspense fallback={<p>Loading Data . . . . .</p>}>
                 <OnePage/>
