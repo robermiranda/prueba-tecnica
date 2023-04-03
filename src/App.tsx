a6db3bad07fa595f7fe2564c833adcb6473d9f82
Rober Miranda <rob.mirandam@gmail.com>
2023-04-02 22:16:16 -0600

    Deleting routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
 import { Suspense } from 'react';
 import {RecoilRoot} from 'recoil';
-//import BankList from './components/BankList';
 import { Box } from '@mui/material';
-import BankCard from './components/BankCard';
-import Home from './components/Home';
-import Page404 from './components/Page404';
 import OnePage from './components/OnePage';
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes } from "react-router-dom";
 
-
-function App() {
-    return (
-        <Router>
-            <Box mx={2} mt={3}>
-            <RecoilRoot>
+export default function App() {
+    return <Box mx={2} mt={3}>
+        <RecoilRoot>
             <Suspense fallback={<p>Loading Data . . . . .</p>}>
-                <Routes>
-                    <Route path='/' element={ <Home/> }/>
-                    <Route path='/bankList' element={ <OnePage/> }/>
-                    <Route path='/bank/:id' element={ <BankCard/> }/>
-                    <Route path='*' element={ <Page404/> }/>
-                </Routes>
+                <OnePage/>
             </Suspense>
-            </RecoilRoot>
-            </Box>
-        </Router>
-    );
+        </RecoilRoot>
+    </Box>
 }
-
-export default App;
