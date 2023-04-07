053440969f9b52a04eaa7c90da0178a280178d7d
Rober Miranda <rob.mirandam@gmail.com>
2023-04-06 23:37:51 -0600

    Add bank0

--- src/util/bankTypes.ts
+++ src/util/bankTypes.ts
@@ -1,8 +1,20 @@
 
 export type bankT = {
-    id?: string;
+    id: string;
     description: string;
     age: number;
     url: string;
     bankName: string;
+    color: string;
+    logo: string;
+}
+
+export const bank0 = {
+    id: '',
+    description: '',
+    age: 0,
+    url: '',
+    bankName: '',
+    color: '',
+    logo: ''
 }
