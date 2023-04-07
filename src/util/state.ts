324b139406db3152a9f2c6d227c2eccf860a5f38
Rober Miranda <rob.mirandam@gmail.com>
2023-04-06 23:37:19 -0600

    Adding bank0

--- src/util/state.ts
+++ src/util/state.ts
@@ -1,6 +1,13 @@
-import { bankT } from './bankTypes';
+import { bankT, bank0 } from './bankTypes';
 import fetchBankList from '../util/net';
-import { selector } from 'recoil';
+import { atom, selector } from 'recoil';
+
+
+export const bankState = atom<bankT> ({
+    key: 'bankState',
+    default: bank0
+});
+
 
 const bankListFetched = selector<bankT[]> ({
     key: 'bankListFetched',
@@ -16,7 +23,35 @@ export const bankListState = selector<bankT[]>({
     get: ({get}) => {
         const bankList = get(bankListFetched);
         return bankList.map(bank => {
-            return {id: bank.bankName.replaceAll(' ', '_'), ...bank}
+            return {...bank, id: bank.bankName.replaceAll(' ', '_')}
+        })
+        .map(bank => {
+            return {...bank, color: getColor(bank.id)}
+        })
+        .map(bank => {
+            return {...bank, logo: getLogo(bank.id)}
         });
     }
 });
+
+function getColor (id: string): string {
+    switch (id.toUpperCase()) {
+        case 'PAGA_TODO': return '#235d4f';
+        case 'BBVA_BANCOMER': return '#0C2453';
+        case 'SCOTIABANK_MÉXICO': return '#ef3c2c';
+        case 'CITIBANAMEX': return '#066cae';
+        case 'BANREGIO': return '#fb6d2f';
+        default: return '#1c2833';
+    }
+}
+
+function getLogo (id: string): string {
+    switch (id.toUpperCase()) {
+        case 'PAGA_TODO': return 'pagaTodo.png';
+        case 'BBVA_BANCOMER': return 'bbva.png';
+        case 'SCOTIABANK_MÉXICO': return 'scotiabank.png';
+        case 'CITIBANAMEX': return 'banamex.jpeg';
+        case 'BANREGIO': return 'banregio.png';
+        default: return 'logo192.png';
+    }
+}
